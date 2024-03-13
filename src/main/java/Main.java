50cf4e6ef0a11d13ff7aad1e84fddd530374f0f2
MateuszTyniec <161605903+MateuszTyniec@users.noreply.github.com>
2024-03-13 13:34:33 +0000

    cccc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,8 @@ public class Main
     System.out.println("Hello World! \nH \ne \nl \nl \no \nw \no \nr \nl \nd");
     String  nazwa = "\nHELLO WORLD";
     for (int i = 0; i < nazwa.length(); i++)
-      {
+      { 
+          
         System.out.println(nazwa.charAt(i));
       }
       
